fd0040610d202786f2ec5078fa182c37b8aea881
trizko <trizko>
2014-11-21 17:20:55 -0800

    Creates and exports db connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,8 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-
+module.exports = mysql.createConnection({
+  user: "root",
+  password: "",
+  database: "chat"
+});
